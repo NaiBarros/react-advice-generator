2d882788f573ddc129829a8675e9849d83086bbc
Naiara Barros <naiara.b.barros@gmail.com>
2023-02-13 17:52:19 -0300

    Initial Commmit

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import divider from "../../assets/pattern-divider-desktop.svg"
+
+function App() {
+  return (
+    <div>
+      <h1>
+        ADVICE #117
+      </h1>
+      <p> dhksjahjhdsjakhjhsjhkahashdajs</p>
+      <img src={divider} alt="divisor da página"/>
+    </div>
+  );
+}
+
+export default App;
