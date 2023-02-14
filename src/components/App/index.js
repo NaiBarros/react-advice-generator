2b4b20435ceef4775f581b60800b9451b92983e7
Naiara Barros <naiara.b.barros@gmail.com>
2023-02-13 23:10:33 -0300

    Finish layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,18 @@
-import divider from "../../assets/pattern-divider-desktop.svg"
+import styles from './styles.module.scss'
+import dividerImg from "../../assets/pattern-divider-desktop.svg"
+import botaoImg from "../../assets/icon-dice.svg"
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>
         ADVICE #117
       </h1>
       <p> dhksjahjhdsjakhjhsjhkahashdajs</p>
-      <img src={divider} alt="divisor da página"/>
+      <img src={dividerImg} alt="divisor da página"/>
+      <button>
+        <img src={botaoImg} alt="imagem do botão"/>
+      </button>
     </div>
   );
 }
