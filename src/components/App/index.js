b9103a13fa53bb796c105fc5ecee9414d9386207
Naiara Barros <naiara.b.barros@gmail.com>
2023-02-22 18:53:24 -0300

    Update CSS

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,13 +6,18 @@ import { useEffect, useState } from "react";
 function App() {
   const [text, setText] = useState("");
   const [id, setId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function getText() {
+    setIsLoading(true);
+
     const response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
+
     console.log(data);
     setText(data.slip.advice);
     setId(data.slip.id);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -24,7 +29,7 @@ function App() {
       <h1>ADVICE #{id}</h1>
       <p>{text}</p>
       <img src={dividerImg} alt="divisor da página" />
-      <button onClick={getText}>
+      <button onClick={getText} disabled={isLoading}>
         <img src={botaoImg} alt="imagem do botão" />
       </button>
     </div>
